6fc139e6a62f21e233aaf734122f988a801be1ec
thiranattada <thiranattada.n@muze.co.th>
2024-05-23 15:36:32 +0700

    Submit primitive values exercise

--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,11 @@
 // Exercise #3: Uploading Progress
+// ให้ Declare ตัว Variable ที่มีชื่อว่า uploadProgress
+// ให้ Assign ตัว Value ของ uploadProgress โดยการเขียน Expression คำนวณหา Progress ของการ Upload File ออกมาเป็นเปอร์เซ็นต์ Notes
+// ขนาดไฟล์ทั้งหมด 250000 Bytes
+// Upload ไฟล์ไปแล้ว 30000 Bytes
 
 let totalFileSize = 250000;
 let uploadedSize = 30000;
-
+let uploadProgress = (uploadedSize/totalFileSize) * 100;
+console.log(uploadProgress);
 // Start coding here
--- ex-4.js
+++ ex-4.js
@@ -1,5 +1,12 @@
 // Exercise #4: E-commerce logic bug
 // Close Quokka before do this!
+// เมื่อ Code ด้านล่างทำงาน ในบรรทัดที่ Highlight สีส้ม จะแสดงผลอย่างไร
+// ⚠️ ลองคิดดูก่อนใช้ Quokka แสดงผลการทำงานของโปรแกรม
+// Copy
+// // Exercise #4: E-commerce logic bug
+// let numberOfSoldProduct = 12000;
+// let numberOfProductInStock = undefined;
+// console.log((numberOfSoldProduct / numberOfProductInStock) * 100);
 
 let numberOfSoldProduct = 12000;
 let numberOfProductInStock = undefined;
